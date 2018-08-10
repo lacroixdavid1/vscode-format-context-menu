0cf6dbd9fd1dd846f27816e3bbcb237514479fd5
David Lacroix <lacroix.david1@gmail.com>
2018-08-10 18:30:57 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# vscode-format-menu
-VSCode extension format multiple files with right click context menu
+# vscode-format-menu README
+
+VSCode extension to format multiple files with right click context menu.
+
+## Features
+
+Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+
+For example if there is an image subfolder under your extension project workspace:
+
+\!\[feature X\]\(images/feature-x.png\)
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `formatContextMenu.view.saveAfterFormat`: enable/disable saving after formatting
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+### 1.0.0
+
+Initial release
+
+
+-----------------------------------------------------------------------------------------------------------
+
+## Working with Markdown
+
+**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,60 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    const formatUris = async (uris: vscode.Uri[]) => {
+        const formatAfterSave = vscode.workspace.getConfiguration().get('formatContextMenu.view.saveAfterFormat') as boolean;
+        for (let i = 0; i < uris.length; i++) {
+            const uri = uris[i];
+            try {
+                await vscode.window.showTextDocument(uris[i], { preserveFocus: false, preview: true });
+                await vscode.commands.executeCommand('editor.action.formatDocument', uri);
+                if (formatAfterSave) {
+                    await vscode.commands.executeCommand('workbench.action.files.save', uri);
+                }
+            } catch (exception) {
+                vscode.window.showWarningMessage(`Could not format file ${uri}`);
+            }
+        }
+    };
+
+    const getRecursiveUris = async (uris: vscode.Uri[]) => {
+        let outputUris: vscode.Uri[] = [];
+        for (let i = 0; i < uris.length; i++) {
+            if (fs.lstatSync(uris[i].fsPath).isDirectory()) {
+                outputUris = [...outputUris, ...await vscode.workspace.findFiles({
+                    base: uris[i].path,
+                    pattern: '**/*'
+                })];
+            } else {
+                outputUris.push(uris[i]);
+            }
+        }
+        return outputUris;
+    };
+
+    context.subscriptions.push(...[
+
+        vscode.commands.registerCommand('extension.formatSelectedFilesFromScmContext', async (...selectedFiles: vscode.SourceControlResourceState[]) => {
+            const uris = await getRecursiveUris(selectedFiles.map(x => x.resourceUri));
+            await formatUris(uris);
+        }),
+
+        vscode.commands.registerCommand('extension.formatSelectedFileFromEditorTileContext', async (selectedFile: vscode.Uri) => {
+            await formatUris([selectedFile]);
+        }),
+
+        vscode.commands.registerCommand('extension.formatSelectedFilesFromExplorerContext', async (clickedFile: vscode.Uri, selectedFiles: vscode.Uri[]) => {
+            const uris = await getRecursiveUris(selectedFiles ? selectedFiles : [clickedFile]);
+            await formatUris(uris);
+        })
+
+    ]);
+
+}
+
+export function deactivate() {
+}
